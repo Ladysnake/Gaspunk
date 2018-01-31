dfab2a858b33835bbfd921414679f48c6998a6ac
CobraDarkPoney <CobraDarkPoney@gmail.com>
2018-01-31 16:14:11 +0100

    Update chlorine texture, improve grenade creative behavior, double spawned particles

--- src/main/java/ladysnake/gaspunk/client/ClientProxy.java
+++ src/main/java/ladysnake/gaspunk/client/ClientProxy.java
@@ -38,7 +38,7 @@ public class ClientProxy extends CommonProxy {
                 double posX = x + world.rand.nextGaussian() * radX % radX;
                 double posY = y + world.rand.nextGaussian() * radY % radY;
                 double posZ = z + world.rand.nextGaussian() * radZ % radZ;
-                ParticleManager.INSTANCE.addParticle(new ParticleGasSmoke(world, posX, posY, posZ, r, g, b, a, (float) (6 + world.rand.nextGaussian())));
+                ParticleManager.INSTANCE.addParticle(new ParticleGasSmoke(world, posX, posY, posZ, r, g, b, a, (float) (25 + world.rand.nextGaussian())));
             }
         }
     }
