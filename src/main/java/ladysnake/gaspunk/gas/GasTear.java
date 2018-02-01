a2b3e39bf442204d6faa268334da93ff8d86f3f9
Fabien <redstoneinfire@gmail.com>
2018-02-01 09:31:49 +0100

    Fix smokes and gastear using vapor texture

--- src/main/java/ladysnake/gaspunk/gas/GasTear.java
+++ src/main/java/ladysnake/gaspunk/gas/GasTear.java
@@ -2,6 +2,7 @@ package ladysnake.gaspunk.gas;
 
 import ladysnake.gaspunk.gas.core.GasTypes;
 import ladysnake.gaspunk.gas.core.IBreathingHandler;
+import ladysnake.gaspunk.gas.core.IGas;
 import ladysnake.gaspunk.network.PacketHandler;
 import ladysnake.gaspunk.network.ShaderMessage;
 import net.minecraft.entity.EntityLivingBase;
@@ -22,6 +23,11 @@ public class GasTear extends Gas {
                 PacketHandler.NET.sendTo(new ShaderMessage("shaders/post/blur.json"), (EntityPlayerMP) entity);
             entity.addPotionEffect(new PotionEffect(MobEffects.SLOWNESS, 5));
         }
+    }
+
+    @Override
+    public ParticleTypes getParticleType() {
+        return ParticleTypes.TEARGAS;
     }
 
     @Override
