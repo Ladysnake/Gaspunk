a0554ce877813925babd712f797c524f146634da
Fabien <redstoneinfire@gmail.com>
2018-02-21 15:46:27 +0100

    Add connection to ladysnake reward page

--- src/main/java/ladysnake/gaspunk/gas/core/IGas.java
+++ src/main/java/ladysnake/gaspunk/gas/core/IGas.java
@@ -10,10 +10,20 @@ public interface IGas extends IForgeRegistryEntry<IGas> {
 
     IGasType getType();
 
+    /**
+     * @return true if this gas triggers asphyxia
+     */
     default boolean isToxic() {
         return getType().isToxic();
     }
 
+    /**
+     * Called each tick to affect entities inside a gas cloud
+     * @param entity the entity breathing this gas
+     * @param handler the entity's breathing handler
+     * @param concentration the concentration of this gas in the air breathed by the entity
+     * @param firstTick true if this entity was not affected by this gas during the previous tick
+     */
     default void applyEffect(EntityLivingBase entity, IBreathingHandler handler, float concentration, boolean firstTick) {
         // NO-OP
     }
