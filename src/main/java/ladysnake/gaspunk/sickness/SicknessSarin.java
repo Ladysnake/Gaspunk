e702a87b07684d284ba3b0d470348c6c57beffed
Fabien <redstoneinfire@gmail.com>
2018-03-18 23:49:36 +0100

    Remove buggy nerve breath modifier

--- src/main/java/ladysnake/gaspunk/sickness/SicknessSarin.java
+++ src/main/java/ladysnake/gaspunk/sickness/SicknessSarin.java
@@ -1,18 +1,14 @@
 package ladysnake.gaspunk.sickness;
 
-import ladysnake.gaspunk.gas.core.CapabilityBreathing;
 import ladysnake.pathos.api.SicknessEffect;
 import net.minecraft.entity.EntityLivingBase;
-import net.minecraft.entity.ai.attributes.AttributeModifier;
 import net.minecraft.util.DamageSource;
 import net.minecraft.util.math.MathHelper;
 
-import java.util.UUID;
-
 public class SicknessSarin extends SicknessGas {
 
-    public static final UUID SARIN_BREATH_PENALTY_ID = UUID.fromString("3f3d3c77-5c3e-4beb-bb0c-02c58bf580cb");
-    public static final AttributeModifier SARIN_BREATH_PENALTY = new AttributeModifier(SARIN_BREATH_PENALTY_ID, "Sarin air supply penalty", -0.5, 1);
+//    public static final UUID SARIN_BREATH_PENALTY_ID = UUID.fromString("3f3d3c77-5c3e-4beb-bb0c-02c58bf580cb");
+//    public static final AttributeModifier SARIN_BREATH_PENALTY = new AttributeModifier(SARIN_BREATH_PENALTY_ID, "Sarin air supply penalty", -0.5, 1);
 
     public SicknessSarin() {
         super(0.01f);
@@ -22,8 +18,8 @@ public class SicknessSarin extends SicknessGas {
     public boolean performEffect(EntityLivingBase carrier, SicknessEffect effect) {
         super.performEffect(carrier, effect);
         if (!carrier.world.isRemote) {
-            if (carrier.getEntityAttribute(CapabilityBreathing.MAX_AIR_SUPPLY).getModifier(SARIN_BREATH_PENALTY_ID) == null)
-                carrier.getEntityAttribute(CapabilityBreathing.MAX_AIR_SUPPLY).applyModifier(SARIN_BREATH_PENALTY);
+//            if (carrier.getEntityAttribute(CapabilityBreathing.MAX_AIR_SUPPLY).getModifier(SARIN_BREATH_PENALTY_ID) == null)
+//                carrier.getEntityAttribute(CapabilityBreathing.MAX_AIR_SUPPLY).applyModifier(SARIN_BREATH_PENALTY);
             if (effect.getTicksSinceLastPerform() % 20 == 0) {
                 carrier.attackEntityFrom(DamageSource.DROWN, MathHelper.ceil(effect.getSeverity() * 10));
                 return true;
@@ -34,6 +30,6 @@ public class SicknessSarin extends SicknessGas {
 
     @Override
     public void onCured(SicknessEffect sicknessEffect, EntityLivingBase carrier) {
-        carrier.getEntityAttribute(CapabilityBreathing.MAX_AIR_SUPPLY).removeModifier(SARIN_BREATH_PENALTY);
+//        carrier.getEntityAttribute(CapabilityBreathing.MAX_AIR_SUPPLY).removeModifier(SARIN_BREATH_PENALTY);
     }
 }
