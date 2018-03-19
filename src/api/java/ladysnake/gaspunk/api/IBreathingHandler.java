6928d39b345d76f55aad5987072fb77cb17d3f9c
Fabien <redstoneinfire@gmail.com>
2018-03-19 22:18:32 +0100

    Fix potion gases being invisible

--- src/api/java/ladysnake/gaspunk/api/IBreathingHandler.java
+++ src/api/java/ladysnake/gaspunk/api/IBreathingHandler.java
@@ -6,7 +6,8 @@ public interface IBreathingHandler {
 
     /**
      * Sets the concentration of the given gas in the target's environment for this tick
-     * @param gas a gas being breathed by this handler
+     *
+     * @param gas           a gas being breathed by this handler
      * @param concentration a <code>float</code> between 0 and 1 representing the gas' concentration
      */
     void setConcentration(IGas gas, float concentration);
@@ -16,12 +17,14 @@ public interface IBreathingHandler {
     void tick();
 
     /**
-     * Returns whether the entity is immune to gases. Currently, this prevents every gas from ticking.
-     * <b>The exact effect of this method is likely to change in future updates</b>
-     * @return true if this handler is wearing a gas mask
+     * Returns whether the entity is immune to the given gas.
+     *
+     * @param gas the type of gas affecting the entity
+     * @param concentration the concentration of the gas in the air surrounding the entity
+     * @return true if this handler is immune to the given gas under the given concentration
      * @see ladysnake.gaspunk.api.event.GasEvent.GasImmunityEvent
      */
-    boolean isImmune();
+    boolean isImmune(IGas gas, float concentration);
 
     float getAirSupply();
 
--- src/main/java/ladysnake/gaspunk/gas/core/GasFactories.java
+++ src/main/java/ladysnake/gaspunk/gas/core/GasFactories.java
@@ -7,7 +7,7 @@ import net.minecraft.potion.Potion;
 public class GasFactories {
 
     public static Gas createGasPotion(Potion potion, int potionDuration, int potionAmplifier) {
-        return new Gas(GasTypes.GAS, potion.getLiquidColor(), GasAgents.createPotionAgent(potion, potionDuration, potionAmplifier), 1.0F);
+        return new Gas(GasTypes.GAS, 0xFF000000 | potion.getLiquidColor(), GasAgents.createPotionAgent(potion, potionDuration, potionAmplifier), 1.0F);
     }
 
 }
