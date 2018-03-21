6b8aba484cf0afce91cb484d56a71b268a969980
Fabien <redstoneinfire@gmail.com>
2018-03-21 15:38:24 +0100

    Make gas clouds stick to their emission point

--- src/main/java/ladysnake/gaspunk/init/ModEntities.java
+++ src/main/java/ladysnake/gaspunk/init/ModEntities.java
@@ -2,11 +2,11 @@ package ladysnake.gaspunk.init;
 
 import ladysnake.gaspunk.GasPunk;
 import ladysnake.gaspunk.client.render.RenderGasCloud;
-import ladysnake.gaspunk.client.render.RenderGasTube;
+import ladysnake.gaspunk.client.render.RenderGrenade;
 import ladysnake.gaspunk.entity.EntityGasCloud;
-import ladysnake.gaspunk.entity.EntityGasTube;
 import ladysnake.gaspunk.entity.EntityGrenade;
 import net.minecraft.client.Minecraft;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.event.ModelRegistryEvent;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.client.registry.RenderingRegistry;
@@ -14,8 +14,11 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.registry.EntityEntry;
 import net.minecraftforge.fml.common.registry.EntityEntryBuilder;
+import net.minecraftforge.fml.common.registry.ForgeRegistries;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
+
+import java.util.Objects;
 
 @Mod.EventBusSubscriber(modid = GasPunk.MOD_ID)
 public final class ModEntities {
@@ -33,14 +36,6 @@ public final class ModEntities {
                         .build(),
 
                 EntityEntryBuilder.create()
-                        .entity(EntityGasTube.class)
-                        .factory(EntityGasTube::new)
-                        .name("gas_tube")
-                        .id("gas_tube", id++)
-                        .tracker(64, 1, true)
-                        .build(),
-
-                EntityEntryBuilder.create()
                         .entity(EntityGasCloud.class)
                         .factory(EntityGasCloud::new)
                         .name("gas_cloud")
@@ -51,10 +46,18 @@ public final class ModEntities {
     }
 
     @SubscribeEvent
+    public static void onRegistryMissingMappings(RegistryEvent.MissingMappings<EntityEntry> event) {
+        for (RegistryEvent.MissingMappings.Mapping<EntityEntry> mapping : event.getMappings()) {
+            if (mapping.key.equals(new ResourceLocation("gaspunk:gas_tube")))
+                mapping.remap(Objects.requireNonNull(ForgeRegistries.ENTITIES.getValue(new ResourceLocation("gaspunk:gas_grenade"))));
+        }
+    }
+
+
+    @SubscribeEvent
     @SideOnly(Side.CLIENT)
     public static void onModelRegistry(ModelRegistryEvent event) {
         RenderingRegistry.registerEntityRenderingHandler(EntityGasCloud.class, RenderGasCloud::new);
-        RenderingRegistry.registerEntityRenderingHandler(EntityGasTube.class, manager -> new RenderGasTube(manager, ModItems.GAS_TUBE, Minecraft.getMinecraft().getRenderItem()));
-        RenderingRegistry.registerEntityRenderingHandler(EntityGrenade.class, manager -> new RenderGasTube(manager, ModItems.GRENADE, Minecraft.getMinecraft().getRenderItem()));
+        RenderingRegistry.registerEntityRenderingHandler(EntityGrenade.class, manager -> new RenderGrenade(manager, ModItems.GRENADE, Minecraft.getMinecraft().getRenderItem()));
     }
 }
