053bf3b358a4dce7df199d75658105dff393d937
sschr15 <steven.sschr15@hotmail.com>
2019-11-25 19:23:34 -0600

    Revert "Magical Cleanup"
    This reverts commit d8e4dd5934a5490500bf127f9ce0bd4d2ce1b87e.

--- src/main/java/ladysnake/gaspunk/network/SpecialRewardMessageHandler.java
+++ src/main/java/ladysnake/gaspunk/network/SpecialRewardMessageHandler.java
@@ -1,6 +1,7 @@
 package ladysnake.gaspunk.network;
 
 import ladysnake.gaspunk.util.SpecialRewardChecker;
+import net.minecraftforge.fml.common.FMLCommonHandler;
 import net.minecraftforge.fml.common.network.simpleimpl.IMessage;
 import net.minecraftforge.fml.common.network.simpleimpl.IMessageHandler;
 import net.minecraftforge.fml.common.network.simpleimpl.MessageContext;
